d0825a17de02219ed83695302376a8b47285819c
7eoz <thesandrini@gmail.com>
2023-03-02 01:28:38 -0300

    isolating all json init structure in a single component file

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,24 +8,6 @@ sap.ui.define(
 	function (Controller, MessageToast, JSONModel, ResourceModel) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.walkthrough.App", {
-			onInit: function () {
-				//set the data model on the view
-				var oData = {
-					recipient: {
-						name: "UI5",
-					},
-				};
-				var oModel = new JSONModel(oData);
-				this.getView().setModel(oModel);
-				//set i18n model on view
-				var i18nModel = new ResourceModel({
-					bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-					supportedLocales: [""],
-					fallbackLocale: "",
-				});
-				this.getView().setModel(i18nModel, "i18n");
-			},
-
 			onShowHello: function () {
 				//read msg from i18n model
 				var oBundle = this.getView().getModel("i18n").getResourceBundle();
--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,14 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
-	"use strict";
+sap.ui.define(
+	["sap/ui/core/ComponentContainer"],
+	function (ComponentContainer) {
+		"use strict";
 
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App",
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-});
+		new ComponentContainer({
+			name: "sap.ui.demo.walkthrough",
+			settings: {
+				id: "walkthrough",
+			},
+			async: true,
+		}).placeAt("content");
+	}
+);
