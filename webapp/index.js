39008a16b97cc583dbf1fcc3863fc343766af52b
7eoz <thesandrini@gmail.com>
2023-03-01 23:56:23 -0300

    calling text from index.js as a real UI5 component

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,7 @@
-sap.ui.define([], function () {
+sap.ui.define(["sap/m/Text"], function (Text) {
 	"use strict";
 
-	// alert("UI5 is ready to go!");
+	new Text({
+		text: "Hello UI5 again!",
+	}).placeAt("content");
 });
