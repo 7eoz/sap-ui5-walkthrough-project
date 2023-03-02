da809367b16ddc01a19404f5074b2d3e46354717
7eoz <thesandrini@gmail.com>
2023-03-02 01:01:44 -0300

    adding a translate file and consuming it in controller toast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
 		"sap/ui/core/mvc/Controller",
 		"sap/m/MessageToast",
 		"sap/ui/model/json/JSONModel",
+		"sap/ui/model/resource/ResourceModel",
 	],
-	function (Controller, MessageToast, JSONModel) {
+	function (Controller, MessageToast, JSONModel, ResourceModel) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.walkthrough.App", {
 			onInit: function () {
@@ -16,10 +17,24 @@ sap.ui.define(
 				};
 				var oModel = new JSONModel(oData);
 				this.getView().setModel(oModel);
+				//set i18n model on view
+				var i18nModel = new ResourceModel({
+					bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+					supportedLocales: [""],
+					fallbackLocale: "",
+				});
+				this.getView().setModel(i18nModel, "i18n");
 			},
 
 			onShowHello: function () {
-				MessageToast.show("Hello There!");
+				//read msg from i18n model
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView()
+					.getModel()
+					.getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				//show message
+				MessageToast.show(sMsg);
 			},
 		});
 	}
