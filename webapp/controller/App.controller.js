d2854b3656b47d89486f9261284363b0d76f0e2f
7eoz <thesandrini@gmail.com>
2023-03-03 00:19:55 -0300

    Separating fiori elements in different view component files

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,4 @@
-sap.ui.define(
-	[
-		"sap/ui/core/mvc/Controller",
-		"sap/m/MessageToast",
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/model/resource/ResourceModel",
-	],
-	function (Controller, MessageToast, JSONModel, ResourceModel) {
-		"use strict";
-		return Controller.extend("sap.ui.demo.walkthrough.App", {
-			onShowHello: function () {
-				//read msg from i18n model
-				var oBundle = this.getView().getModel("i18n").getResourceBundle();
-				var sRecipient = this.getView()
-					.getModel()
-					.getProperty("/recipient/name");
-				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-				//show message
-				MessageToast.show(sMsg);
-			},
-		});
-	}
-);
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.App", {});
+});
